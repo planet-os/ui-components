23ccb8e9d6a13db980c6adb3b06f23304cfcf536
Chris Viau <christopheviau@gmail.com>
2017-04-12 16:22:01 -0400

    0.8.17

--- datahub-ui/dist/datahub.js
+++ datahub-ui/dist/datahub.js
@@ -1579,6 +1579,12 @@
             var paletteScale = d3.scaleLinear().domain([ 0, sliceCount - 1 ]).range(palette);
             return d3.range(sliceCount).map(paletteScale);
         };
+        var sliceDomain = function(values, sliceCount) {
+            var stepWidth = (d3.max(values) - d3.min(values)) / sliceCount;
+            return d3.range(sliceCount).map(function(d, i) {
+                return d3.min(values) + i * stepWidth;
+            });
+        };
         var colorScales = {
             grayscale: function(values) {
                 return d3.scaleLinear().domain(d3.extent(values)).range([ "white", "black" ]);
@@ -1588,6 +1594,20 @@
             },
             equalizedGrayscale: function(values) {
                 return equalize(values, slicePalette([ "white", "black" ], 10));
+            },
+            reversedBrewerSpectral: function(values) {
+                if (d3.min(values) >= 0) {
+                    return d3.scaleLinear().domain(sliceDomain(values, brewerSpectral.length)).range(brewerSpectral.slice().reverse());
+                }
+                var negative = sliceDomain(values.filter(function(d) {
+                    return d <= 0;
+                }), brewerSpectral.length / 2 - 1);
+                var positive = sliceDomain(values.filter(function(d) {
+                    return d > 0;
+                }), brewerSpectral.length / 2 - 1);
+                var domainValues = negative.concat([ 0 ]).concat(positive);
+                var colorScale = brewerSpectral.slice().reverse();
+                return d3.scaleLinear().domain(domainValues).range(colorScale);
             }
         };
         dh.palette = colorScales;
