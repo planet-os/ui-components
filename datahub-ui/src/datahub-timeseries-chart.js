1ef54ad5940640375b43979169ac6d6bb92badd6
MayGo <maigo.erit@gmail.com>
2018-05-30 14:01:28 +0300

    Add passive attribute to touchstart

--- datahub-ui/src/datahub-timeseries-chart.js
+++ datahub-ui/src/datahub-timeseries-chart.js
@@ -520,7 +520,7 @@
         var dataUnderCursor = getHoverInfo(config, mouseTimestamp);
 
         config.events.call("hover", null, dataUnderCursor);
-      })
+      }, { passive: true })
       .on("mouseout", function (d) {
         hideTooltip(config);
         config.container.selectAll(".axis-title.x text").text(null);
